43bb9fa6ebbb7faeddf882347f4049fa99afef2a
ByronG <byrong.gt@gmail.com>
2023-05-25 10:15:35 -0600

    Actualizado el gráfico de predichos vs residuos

--- arreglo en parcelas divididas/labranza_control_malezas_mmixto.R
+++ arreglo en parcelas divididas/labranza_control_malezas_mmixto.R
@@ -14,41 +14,32 @@ dataset <- dataset %>%
   mutate(across(c(Tillage, WeedControl, Block), .fns = factor))
 head(dataset)
 
-mod.aov <- aov(Yield ~ Tillage*WeedControl +
-                 Error(Block/Tillage), data = dataset)
-
-summary(mod.aov)
-
-mod.aov2 <- aov(Yield ~ Block + Tillage*WeedControl +
+# Andeva con efectos fijos
+mod.aov <- aov(Yield ~ Block + Tillage*WeedControl +
                   Error(Block:Tillage), data = dataset)
-summary(mod.aov2)
+summary(mod.aov)
 
 # Andeva con modelos mixtos
 # Considerar la labranza como efecto aleatorio
 
-if(!require(nlme)){install.packages("nlme")}
-
-mod.lme <- lme(Yield ~ Block + Tillage*WeedControl,
-               random = ~1|Block/Tillage, data = dataset)
-anova(mod.lme)
-
 if(!require(lme4)){install.packages("lme4")}
 if(!require(lmerTest)){install.packages("lmerTest")}
 
-mod.lmer.split <- lmer(Yield ~ Block + WeedControl*Tillage +
+mod.mixto <- lmer(Yield ~ Block + WeedControl*Tillage +
                          (1|Block:Tillage), 
                        data=dataset)
 # Revisar el cumplimiento de los supuestos
-shapiro.test(residuals(mod.lmer.split))
+plot(mod.mixto)
+shapiro.test(residuals(mod.mixto))
 
 
-anova(mod.lmer.split, ddf = "Kenward-Roger")
+anova(mod.split, ddf = "Kenward-Roger")
 
 # Dado que la interacción es significativa, se solicita 
 # el procedimiento emmeans
 
 if(!require(emmeans)){install.packages("emmeans")}
-meansAB <- emmeans(mod.lmer.split, ~Tillage:WeedControl)
+meansAB <- emmeans(mod.mixto, ~Tillage:WeedControl)
 multcomp::cld(meansAB, Letters = LETTERS)
 
 
