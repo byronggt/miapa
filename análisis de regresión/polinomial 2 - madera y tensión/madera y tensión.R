913af304a6ceb07989487e2276546c32e79d2bb5
ByronG <byrong.gt@gmail.com>
2023-06-03 10:18:45 -0600

    Actualizado modelo polinomial de grado 2

--- análisis de regresión/polinomial 2 - madera y tensión/madera y tensión.R
+++ análisis de regresión/polinomial 2 - madera y tensión/madera y tensión.R
@@ -2,20 +2,26 @@
 # Dr. Byron González
 # Dr. Ezequiel López
 # http://cete.fausac.gt
+
+if(!require(performance)){install.packages("performance")}
+if(!require(tidyverse)){install.packages("tidyverse")}
+if(!require(readxl)){install.packages("readxl")}
+
 # Importar el archivo de datos
-library(readxl)
 madera<-read_excel("madera.y.tension.xlsx")
-library(performance)
-library(tidyverse)
+
 #Ajuste de un modelo lineal
 # Note la falta de ajuste
+
 mad1<- lm(tension~madera, data=madera)
 summary(mad1)
 check_model(mad1)
+
 # Ajuste de un modelo polinomial de grado 2
 mad2<- lm(tension~madera+I(madera^2), data=madera)
 summary(mad2)
 check_model(mad2)
+
 # Note que debe eliminarse el intercepto
 mad3<- lm(tension~-1+madera+I(madera^2), data=madera)
 summary(mad3)
