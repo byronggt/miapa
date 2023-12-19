8994c9ed1a6e2b53f2633fee6dec04e6289cbb4e
ByronG <byrong.gt@gmail.com>
2023-12-19 10:41:41 -0600

    Actualizada la aleatorización de strip-plot

---
+++ arreglo en parcelas subdivididas/aleatorizacion_split_split_plot_maiz.R
@@ -0,0 +1,9 @@
+# Dr. Byron González
+# Dr. Ezequiel López
+# http://cete.fausac.gt
+
+# Parcelas subdivididas
+if(!require(FielDHub)){install.packages("FielDHub")}
+run_app()
+# Indicar los niveles de cada factor, número de repeticiones y localidades
+
