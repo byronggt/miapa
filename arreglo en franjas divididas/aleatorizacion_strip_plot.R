f5869712ca30162652f1982f1e04af0f626c3b12
ByronG <byrong.gt@gmail.com>
2023-12-19 10:42:36 -0600

    Actualizada la aleatorización de strip-plot

--- arreglo en franjas divididas/aleatorizacion_strip_plot.R
+++ arreglo en franjas divididas/aleatorizacion_strip_plot.R
@@ -2,7 +2,7 @@
 # Dr. Ezequiel López
 # http://cete.fausac.gt
 
-# Parcelas subdivididas
+# Franjas divididas
 if(!require(FielDHub)){install.packages("FielDHub")}
 run_app()
 # Indicar los niveles de cada factor, número de repeticiones y localidades
