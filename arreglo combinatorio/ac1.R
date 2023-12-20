5b6c48f034bee0111d01ab46e12853f21a39adf8
ByronG <byrong.gt@gmail.com>
2023-12-20 16:17:13 -0600

    Actualizado alpacas

--- arreglo combinatorio/ac1.R
+++
@@ -1,1 +0,0 @@
-# http://cete.fausac.gt
---
+++ arreglo combinatorio/alpacas.R
@@ -0,0 +1,6 @@
+# Dr. Byron González
+# Dr. Ezequiel López
+# http://cete.fausac.gt
+
+if(!require(readxl)){install.packages("readxl")}
+alpacas<-read_excel("peso_alpacas.xlsx")
